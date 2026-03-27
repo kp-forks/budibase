2be8ce756564bc7e3c905c3ba4a9f18367333535
Adrià Navarro Redó <adria@budibase.com>
2026-03-27 15:02:20 +0100

    Cleanup validators

--- packages/server/src/api/routes/utils/validators/knowledgeBase.ts
+++ packages/server/src/api/routes/utils/validators/knowledgeBase.ts
@@ -5,25 +5,11 @@ import Joi from "joi"
 const REQUIRED_STRING = Joi.string().required()
 const KB_TYPE = Joi.string().valid(...Object.values(KnowledgeBaseType))
 
-const LOCAL_KB_CONFIG = Joi.object({
-  embeddingModel: REQUIRED_STRING,
-  vectorDb: REQUIRED_STRING,
-})
-
-const GEMINI_KB_CONFIG = Joi.object({
-  googleFileStoreId: REQUIRED_STRING,
-}).unknown(true)
-
 export function createKnowledgeBaseValidator() {
   return auth.joiValidator.body(
     Joi.object({
       name: REQUIRED_STRING,
-      type: KB_TYPE,
-      config: Joi.when("type", {
-        is: KnowledgeBaseType.GEMINI,
-        then: Joi.forbidden(),
-        otherwise: LOCAL_KB_CONFIG.required(),
-      }),
+      type: KB_TYPE.required(),
     })
   )
 }
@@ -35,13 +21,6 @@ export function updateKnowledgeBaseValidator() {
       _rev: REQUIRED_STRING,
       name: REQUIRED_STRING,
       type: KB_TYPE.required(),
-      config: Joi.when("type", {
-        is: KnowledgeBaseType.GEMINI,
-        then: Joi.alternatives()
-          .try(Joi.object().unknown(true), GEMINI_KB_CONFIG)
-          .optional(),
-        otherwise: LOCAL_KB_CONFIG.required(),
-      }),
     }).unknown(true)
   )
 }
